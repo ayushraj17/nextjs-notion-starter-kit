bd143fd0b535ed68fc5c2c1135028fb5059f85db
ayushraj17 <meayushraj17@gmail.com>
2021-11-08 12:13:52 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 // const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
+const path = require('path')
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
@@ -7,5 +8,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   images: {
     domains: ['pbs.twimg.com']
+  },
+  target: 'serverless',
+  webpack(config) {
+    config.resolve.modules.push(path.resolve('./'))
+    return config
   }
 })
